c76aaa859d35be1faecdaa2099d06d0fda102738
AjayAjishaa <119327934+AjayAjishaa@users.noreply.github.com>
2023-05-18 11:27:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preLoad()
+{
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw()
+{
+}
+
+function take_snapshot()
+{
+    save("myFilterImage.png")
+}
